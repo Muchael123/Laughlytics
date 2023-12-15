242ffda1559cb680a9b4ab394d8c6c00ab8c018a
Muchael123 <mainamichael2849@gmail.com>
2023-12-15 12:16:08 +0300

    add: Add refresh to fetch new jokes after 10s

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,38 +1,32 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function useFetch(url) {
-    const [data, setData] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
-    useEffect(() => {
-         setLoading((prev) => !prev);
-        axios
-        .get(url)
-        .then((response) => {
-           setData(response.data)
-        })
-        .catch((err) => {
-            setError(err);
-        })
-        .finally(() => setLoading(false))
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-    }, [url])
+  const fetchData = () => {
+    setLoading(true);
+    axios
+      .get(url)
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((err) => {
+        setError(err);
+      })
+      .finally(() => setLoading(false));
+  };
 
-    const reFetch = () => {
-         setLoading((prev) => !prev);
-         axios
-           .get(url)
-           .then((response) => {
-             setData(response.data);
-           })
-           .catch((err) => {
-             setError(err);
-           })
-           .finally(() => setLoading(false));
+  useEffect(() => {
+    fetchData(); // Initial fetch
+    const interval = setInterval(fetchData, 10000); // Refresh every 10 seconds
 
-    }
-    return {data, loading, error, reFetch};
+    return () => clearInterval(interval); // Cleanup on unmount
+  }, [url]);
+
+  return { data, loading, error };
 }
 
 export default useFetch;
